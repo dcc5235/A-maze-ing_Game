dd2b0cfb6068246bd71067d7151c1a840d60ea61
Dany <dany@Danys-MacBook-Pro.local>
2020-11-13 18:45:27 -0600

    keypresses for directions tied to asdw keys

--- index.js
+++ index.js
@@ -170,3 +170,22 @@ const ball = Bodies.circle(
   unitLength / 4
 );
 World.add(world, ball);
+
+// Keypresses
+document.addEventListener('keydown', event => {
+  if (event.keyCode === 87) {
+    console.log('move ball up');
+  }
+
+  if (event.keyCode === 68) {
+    console.log('move ball right');
+  }
+
+  if (event.keyCode === 83) {
+    console.log('move ball down');
+  }
+
+  if (event.keyCode === 65) {
+    console.log('move ball left');
+  }
+});
